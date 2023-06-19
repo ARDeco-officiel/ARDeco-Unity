c2338d03ee19c367fb9be20998cda63076065c52
Evanblachier <evan.blachier@epitech.eu>
2023-06-19 22:27:04 +0200

    [-] fix login [-]

--- Assets/Scenes/LoginScene/LoginScript.cs
+++ Assets/Scenes/LoginScene/LoginScript.cs
@@ -12,7 +12,6 @@ public class LoginScript : MonoBehaviour
 {
     public TMP_InputField emailInput;
     public TMP_InputField passwordInput;
-    public Color errorColor = Color.red;
 
     private void Start()
     {
@@ -45,8 +44,6 @@ public class LoginScript : MonoBehaviour
             {
                 UnityEngine.Debug.Log("Error on login : " + webRequest.error);
                 UnityEngine.Debug.Log("Response : " + webRequest.downloadHandler.text);
-                emailInput.image.color = errorColor;
-                passwordInput.image.color = errorColor;
             }
             else
             {
